ed55cbecb93c8b151f5cffa41fea516ef3d2e16c
ANDE RAM SANKAR SIVAJI <siva4725iot@gmail.com>
2025-08-15 19:27:00 +0530

    Update 13-08-25_assignment-day1.py

--- 13-08-25_assignment-day1.py
+++ 13-08-25_assignment-day1.py
@@ -1,24 +1,36 @@
 #1.Reverse a given string without using built-in reverse functions.
-'''string = input()
+string = input()
 rev=''
 for i in range(len(string)-1, -1, -1):
     rev=rev+string[i]
-print (rev)'''
+print (rev)
+
+Output:
+ram shankar sivaji
+ijavis raknahs mar
 
 
 #2. Check if a string is palindrome or not
-'''str=input()
+str=input()
 rev=''
 for i in range(len(str)-1,-1,-1):
     rev=rev+str[i]
 if rev==str:
     print("Palindrome")
 else:
-    print("Not")'''
+    print("Not")
+
+Output_1:
+ram
+Not
+Output_2:
+madam
+Palindrome
+
 
 
 #3.Count the no.of vowels and consonents in a string
-'''str=input()
+str=input()
 str=str.lower()
 vowels_set="aeiou"
 vowels_count=0
@@ -30,16 +42,27 @@ for i in str:
         else:
             consonants_count += 1
 print("Vowels:", vowels_count)
-print("Consonants:", consonants_count)'''
+print("Consonants:", consonants_count)
+
+Output:
+ram shankar sivaji
+Vowels: 6
+Consonants: 10
 
 
 #4.Remove all spaces from a given string
-'''str = input()
+str = input()
 no_spaces = ""
 for i in str:
     if i != " ":   
         no_spaces += i
-print("String without spaces:", no_spaces)'''
+print("String without spaces:", no_spaces)
+
+Output:
+Ram Shankar Sivaji
+String without spaces: RamShankarSivaji
+
+
 
 
 #5.Count the frequency of each character in a string
@@ -53,3 +76,17 @@ for i in str:
 for key, value in freq.items():
     #print(f"'{key}': {value}")
     print(key, ":", value)
+
+Output:
+ram shankar sivaji
+r : 2
+a : 4
+m : 1
+  : 2
+s : 2
+h : 1
+n : 1
+k : 1
+i : 2
+v : 1
+j : 1
